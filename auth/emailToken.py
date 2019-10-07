9686379e829ffb420e2996651795fdf89acf03a7
haji <mr.linux3@yahoo.com>
2019-10-08 00:45:00 +0330

    Add send email fucntion to emailToken file

--- auth/emailToken.py
+++ auth/emailToken.py
@@ -1,24 +1,36 @@
 from itsdangerous import URLSafeTimedSerializer
-from config import app, db
+from config import mail
+from flask import current_app
+from flask_mail import Message
 
 
 def generate_confirmation_token(email):
-    serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    serializer = URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
 
     return serializer.dumps(
         email,
-        salt=app.config['REGISTER_SALT']
+        salt=current_app.config['REGISTER_SALT']
     )
 
 
 def confirm_registeration_token(token):
-    serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    serializer = URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
     try:
         email = serializer.loads(
             token,
-            salt=app.config['REGISTER_SALT'],
-            max_age = app.config['REGISTER_EXPIRATION']
+            salt=current_app.config['REGISTER_SALT'],
+            max_age=current_app.config['REGISTER_EXPIRATION']
         )
     except:
         return False
     return email
+
+
+def send_email(to, subject, template):
+    msg = Message(
+        subject,
+        recipients=[to],
+        html=template,
+        sender=current_app.config['MAIL_DEFAULT_SENDER']
+    )
+    mail.send(msg)
