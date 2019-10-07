1515e72e766c909865764ff139384e24f98a8d44
haji <mr.linux3@yahoo.com>
2019-10-08 00:44:39 +0330

    Add email confirmaition for registeration

--- auth/controllers.py
+++ auth/controllers.py
@@ -4,7 +4,9 @@ from user.models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_jwt_extended import create_access_token, create_refresh_token, get_jwt_identity
 from .parsers import login_parser, registeration_parser
-
+from .emailToken import confirm_registeration_token, generate_confirmation_token, send_email
+from flask import render_template, url_for
+import smtplib
 
 class Login(Resource):
     @api.expect(login_parser)
@@ -70,16 +72,45 @@ class Register(Resource):
             is_admin = False
         )
 
-        db.session.add(user)
-        db.session.commit()
 
+        #generating token
         access_token = create_access_token(
             identity={"username": user.username, 'is_admin': user.is_admin, 'email': user.email})
         refresh_token = create_refresh_token(
             identity={"username": user.username, 'is_admin': user.is_admin, 'email': user.email})
+        
+        #sending email
+        token = generate_confirmation_token(user.email)
+        confirm_url = url_for('confirm_registeration_email', token=token, _external=True)
+        html = render_template('actvate_email.html', confirm_url=confirm_url)
+        try:
+            send_email(user.email, "SE project : Registeration confirmation email", html)
+        except smtplib.SMTPRecipientsRefused:
+            return {'message':'unable to send email to {}'.format(user.email)}
+
+        db.session.add(user)
+        db.session.commit()
 
         return {
-            'message': 'Registration successful',
+            'message': 'Registration successful, confirmation email is sent to your email.',
             'access_token': access_token,
             'refresh_token': refresh_token
         }
+
+
+class RegisterConfirmation(Resource):
+    def get(self, token):
+        try:
+            email = confirm_registeration_token(token)
+        except:
+            return {'message':'Invalid link or expiered link.'}
+        
+        user = User.query.filter_by(email=email).first_or_404()
+
+        if user.is_confirmed:
+            return {'message':'Already confirmed; please login.'}
+        else:
+            user.is_confirmed = True
+            db.session.add(user)
+            db.session.commit()
+            return {'message':'Account confirmed successfully'}
--- router.py
+++ router.py
@@ -1,11 +1,12 @@
 from flask import Flask
-from config import db, api, jwt
+from config import db, api, jwt, mail
 from user.controllers import UserView, UserListView
-from auth.controllers import Login, Register
+from auth.controllers import Login, Register, RegisterConfirmation
 app = Flask(__name__)
 
 #initing app
 app.config.from_pyfile('config.cfg')
+mail.init_app(app)
 db.init_app(app)
 api.init_app(app)
 jwt.init_app(app)
@@ -16,10 +17,14 @@ with app.app_context():
     db.create_all()
 
 #routing the entire project
+
+#routing user
 api.add_resource(UserView, '/user/<int:user_id>')
 api.add_resource(UserListView, '/user')
+#routing authentication
 api.add_resource(Login,'/login')
 api.add_resource(Register, '/registeration')
+api.add_resource(RegisterConfirmation,'/registeration/<token>', endpoint = 'confirm_registeration_email')
 
 if __name__ == '__main__':
     app.run(debug=True)
