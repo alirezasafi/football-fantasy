a00e687740f40c47d9cb7a67688af6c0aab1540a
haji <mr.linux3@yahoo.com>
2019-10-09 00:21:32 +0330

    Create reset password parsers

--- auth/parsers.py
+++ auth/parsers.py
@@ -10,3 +10,8 @@ registeration_parser.add_argument('username', required=True)
 registeration_parser.add_argument('email', required=True)
 registeration_parser.add_argument('password1', required=True)
 registeration_parser.add_argument('password2', required=True)
+
+reset_password_parser = reqparse.RequestParser()
+registeration_parser.add_argument('old_password1', required=True)
+registeration_parser.add_argument('old_password2', required=True)
+registeration_parser.add_argument('new_password', required=True)
