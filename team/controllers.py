c0c4652ac378e6c4f226f0e8916c0867a0f5acd8
alirezaSafi <ali02reza78@gmail.com>
2019-10-24 00:11:53 +0330

    Add player substitution and switch captain

--- team/controllers.py
+++ team/controllers.py
@@ -89,15 +89,83 @@ class ManageTeam(Resource):
     def get(self):
         email = get_jwt_identity()['email']
         user_obj = User.query.filter_by(email=email).first()
-        lineup = user_obj.squad.filter_by(lineup=True)
-        bench = user_obj.squad.filter_by(lineup=False)
-        if len(lineup) == 14 and bench == 4:
+        lineup = user_obj.squad.filter_by(lineup=True).all()
+        bench = user_obj.squad.filter_by(lineup=False).all()
+
+        if len(lineup) == 11 and len(bench) == 4:
             squad = serialize_player(lineup, True)
             squad += serialize_player(bench, False)
             response = make_response(jsonify(squad), 200)
             return response
         response = make_response(jsonify({"detail": "first pick your team!!"}), 400)
         return response
+
+    @api.expect(parsers.ManageTeam_parser)
+    @jwt_required
+    @account_actication_required
+    def put(self):
+        args = parsers.ManageTeam_parser.parse_args()
+        new_user_squad = sorted(args['squad'], key=lambda k: k['player_id'])
+        if validate_squad(new_user_squad):
+            email = get_jwt_identity()['email']
+            user_obj = User.query.filter_by(email=email).first()
+            user_squad = user_obj.squad.order_by(models.Squad.player_id).all()
+            for i in range(15):
+                if new_user_squad[i]['player_id'] == user_squad[i].player_id:
+                    user_squad[i].lineup = new_user_squad[i]['lineup']
+                else:
+                    response = make_response(jsonify({"detail": "400 BAD REQUEST"}), 400)
+                    return response
+
+            user_obj.captain = args['captain-id']
+            db.session.commit()
+            response = make_response(jsonify({"detail": "successfully upgraded"}), 200)
+            return response
+        else:
+            response = make_response(jsonify({"detail": "400 BAD REQUEST"}), 400)
+            return response
+
+
+def validate_squad(squad):
+    GP = 0
+    DF = 0
+    MF = 0
+    FD = 0
+    for player in squad:
+        if player['position'] == 'Goalkeeper':
+            GP += 1
+        elif player['position'] == 'Defender':
+            DF += 1
+        elif player['position'] == 'Midfielder':
+            MF += 1
+        else:
+            FD += 1
+    if not (GP == 2 and DF == 5 and MF == 5 and FD == 3):
+        return False
+    GP = 0
+    DF = 0
+    MF = 0
+    FD = 0
+    formations = [(4,3,3), (4,4,2), (4,5,1), (3,4,3), (3,5,2), (5,4,1)]
+    lineup = 0
+    for player in squad:
+        if player['position'] == 'Goalkeeper' and player['lineup']:
+            GP += 1
+            lineup += 1
+        elif player['position'] == 'Defender' and player['lineup']:
+            DF += 1
+            lineup += 1
+        elif player['position'] == 'Midfielder' and player['lineup']:
+            MF += 1
+            lineup += 1
+        elif player['position'] == 'Forward' and player['lineup']:
+            FD += 1
+            lineup += 1
+    print((DF,MF,FD))
+    if (DF,MF,FD) in formations and GP == 1 and lineup == 11:
+        return True
+    else:
+        return False
 
 
 def serialize_player(squad, in_lineup):
--- team/parsers.py
+++ team/parsers.py
@@ -6,3 +6,7 @@ PickSquad_parser.add_argument('squad-name', required=True)
 PickSquad_parser.add_argument('picks', type=dict, action='append', required=True)
 PickSquad_parser.add_argument('favourite-team')
 PickSquad_parser.add_argument('captain-id', required=True)
+
+ManageTeam_parser = reqparse.RequestParser()
+ManageTeam_parser.add_argument('squad', type=dict, action='append', required=True)
+ManageTeam_parser.add_argument('captain-id', required=True)
