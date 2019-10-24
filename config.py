0fba5d758a6ac461a27530fa67f7c6f4017fde0e
haji <mr.linux3@yahoo.com>
2019-10-24 20:52:51 +0330

    Add detail to main api

--- config.py
+++ config.py
@@ -3,7 +3,11 @@ from flask_restplus import Api
 from flask_jwt_extended import JWTManager
 from flask_mail import Mail
 
-api = Api()
+api = Api(
+    title='Football fantasy api',
+    version='1.0',
+    description='software engineering project',
+)
 db = SQLAlchemy()
 jwt = JWTManager()
 mail = Mail()
