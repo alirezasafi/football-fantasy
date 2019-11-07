fcccc4ec1b534f8853d62a9dbd917f22956897d4
mohammad <mr.linux3@yahoo.com>
2019-11-07 11:00:22 +0330

    Add marshmallow

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_restplus import Api
 from flask_jwt_extended import JWTManager
 from flask_mail import Mail
+from flask_marshmallow import Marshmallow
 
 authorizations = {
     'Bearer Auth': {
@@ -19,6 +20,7 @@ api = Api(
     security='Bearer Auth',
     authorizations=authorizations
 )
+ma = Marshmallow()
 db = SQLAlchemy()
 jwt = JWTManager()
 mail = Mail()
