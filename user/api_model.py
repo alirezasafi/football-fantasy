d64959d6663fb6f114ecf3ebd359794e66ce8cc8
alireza <ali02reza78@gmail.com>
2019-11-08 14:22:31 +0330

    Add profile management

--- user/api_model.py
+++ user/api_model.py
@@ -1,3 +1,17 @@
 from flask_restplus import Namespace, fields
 
-user_api = Namespace("User Management", description="Admin api for manipulating users")
+user_api = Namespace("user management", description="Admin api for manipulating users")
+
+delete_profile_model = user_api.model(
+    'profile',{
+        'password': fields.String(required=True),
+    }
+)
+
+update_profile_model = user_api.model(
+    'profile',{
+        'username': fields.String(required=True),
+        'password': fields.String(required=True),
+        'email':fields.String(required=True),
+    }
+)
