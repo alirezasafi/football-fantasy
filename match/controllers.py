7bce9bbc4e72eedb6d79729d8e16727e8132ec49
mohammad <mr.linux3@yahoo.com>
2019-12-11 22:01:30 +0330

    Update Match List and Match Details to be seperated

--- match/controllers.py
+++ match/controllers.py
@@ -2,8 +2,9 @@ from flask_restplus import Resource
 from .models import Match
 from .api_model import match_api
 import datetime
-from .match_marshmallow import MatchListSchema
+from .match_marshmallow import MatchListSchema, MatchDetailSchema
 from config import db
+from functools import wraps
 
 @match_api.route('/<int:competition_id>/current-week-matches')
 class CurrentWeekMatches(Resource):
@@ -13,12 +14,29 @@ class CurrentWeekMatches(Resource):
 
         startOfWeek = today - datetime.timedelta(days=today.weekday())
         endOfWeek = startOfWeek + datetime.timedelta(days=7)
-        # all_matches = Match.query.filter(db.and_(Match.utcDate >= startOfWeek, Match.competition_id == competition_id)).all()
-        all_matches = Match.query.filter(db.and_(Match.utcDate <= endOfWeek, Match.utcDate >= startOfWeek)).all()
+        all_matches = Match.query.filter(db.and_(Match.competition_id == competition_id, Match.utcDate <= endOfWeek, Match.utcDate >= startOfWeek)).all()
         match_schema = MatchListSchema(many = True)
         output = match_schema.dump(all_matches)
-        return {"Matches":output}
+        status = 200
+        if len(output) == 0:
+            status = 404
+            output = "there are no mathces available."
+        return {"Matches":output}, status
 
 
+
+@match_api.route('/<int:match_id>/details')
+class MatchDetail(Resource):
+    # @match_exsists
+    def get(self, match_id):
+        match = Match.query.filter(Match.id == match_id).first()
+        output = ""
+        status = 200
+        if match == None:
+            output = "Such match is not found."
+            status = 404
+        else:
+            output = MatchDetailSchema().dump(match)
+        return {'match':output}, status
 
 
--- match/match_marshmallow.py
+++ match/match_marshmallow.py
@@ -9,6 +9,15 @@ from game_event.event_marshmallow import EventSchema
 class MatchListSchema(ma.ModelSchema):
     class Meta:
         model = Match
+        exclude = ('players','homeTeamCaptain', 'awayTeamCaptain', 'events')
+    status = EnumField(Status, True)
+    homeTeam = ma.Nested(ClubSchema())
+    awayTeam = ma.Nested(ClubSchema())
+    competition = ma.Nested(CompetitionSchema())
+
+class MatchDetailSchema(ma.ModelSchema):
+    class Meta:
+        model = Match
         exclude = ('players',)
     status = EnumField(Status, True)
     homeTeam = ma.Nested(ClubSchema())
@@ -16,6 +25,4 @@ class MatchListSchema(ma.ModelSchema):
     homeTeamCaptain = ma.Nested(PlayerSchema())
     awayTeamCaptain = ma.Nested(PlayerSchema())
     competition = ma.Nested(CompetitionSchema())
-    # events = 
     events = ma.Nested(EventSchema, many = True)
-
