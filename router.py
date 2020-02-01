ed3a4d41e8535c5c244dee8b1f4c0db8d17d6920
mohammad <mr.linux3@yahoo.com>
2020-02-02 00:54:28 +0330

    ADD celery

--- router.py
+++ router.py
@@ -4,8 +4,11 @@ from user.controllers import UserView, UserListView
 from auth.controllers import Login, Register, RegisterConfirmation, ResetPasswordConfirmation, ResetPassword
 from flask_cors import CORS
 from game_event.models import Event
+from celery import Celery
+from celery.schedules import crontab
+# from celery_scheduler.make_celery import make_celery
 
-#importing apis
+# importing apis
 from auth.api_model import auth_api
 from player.api_model import player_api
 from team.api_model import team_api
@@ -16,7 +19,7 @@ from club.api_model import club_api
 from match.api_model import match_api
 from statistics.api_model import statistics_api
 
-#importing controllers
+# importing controllers
 from team.controllers import PickSquad, ManageTeam
 from database_population.areas_population_controller import PopulateAreas
 from database_population.competition_club_population_controller import PopulateClubsCompetitions
@@ -26,19 +29,20 @@ from player.controllers import MediaPlayer, CompetitionPlayers
 from compeition.controllers import CompetitionListView
 from club.controller import ClubByCompetition
 from database_population.player_score_calculator_controller import PlayerScoreCalc
-from database_population.match_event_update_controller import UpdateMatchEvents
 from team.squad_point_calculator_controller import CalculateSquadPoint
 from match.controllers import CurrentWeekMatches
 from database_population.cards_update_controller import UpdateCards
 from statistics.controllers import PlayerStatistics, SquadStatistics
 from database_population.club_update_controller import UpdateClub
 
+from database_population.match_event_update_controller import perform_update
+
 import os
 
-import logging
+
 app = Flask(__name__)
 
-#initing api
+# initing api
 api.init_app(app)
 api.add_namespace(auth_api, path='/auth')
 api.add_namespace(player_api, path='/player')
@@ -50,7 +54,7 @@ api.add_namespace(club_api, path='/club')
 api.add_namespace(match_api, path='/match')
 api.add_namespace(statistics_api, path='/statistics')
 
-#initing app
+# initing app
 app.config.from_pyfile('config.cfg')
 app.config['MEDIA_ROOT'] = os.path.join(os.path.dirname(__file__), 'media')
 mail.init_app(app)
@@ -59,15 +63,49 @@ jwt.init_app(app)
 ma.init_app(app)
 jwt._set_error_handler_callbacks(api)
 
-#creating new database tables
+# creating new database tables
 with app.app_context():
     db.create_all()
 
-
-CORS(app)#, resources={r"/*": {"origins": "*"}})
+CORS(app)  # , resources={r"/*": {"origins": "*"}})
 
 the_cli = cli.FlaskGroup(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['SWAGGER_BASEPATH'] = '/contacts'
+
+
+def make_celery(app):
+    celery = Celery("mohammad", backend=app.config['CELERY_RESULT_BACKEND'],
+                    broker=app.config['CELERY_BROKER_URL'])
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+
+    class ContextTask(TaskBase):
+        abstract = True
+
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+
+    celery.Task = ContextTask
+    return celery
+
+
+celery = make_celery(app)
+
+
+@celery.task
+def update_match_event():
+    perform_update()
+
+
+@celery.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(
+        crontab(minute='0', hour='5'),
+        update_match_event,
+    )
+
+
 if __name__ == '__main__':
     the_cli()
