967ccf14e0b953c54cec8afd745c354a8bf42264
alirezaSafi <ali02reza78@gmail.com>
2020-04-16 22:52:55 +0430

    Update media player

--- player/controllers.py
+++ player/controllers.py
@@ -1,28 +1,24 @@
-from . import (
-    models
-)
-from flask import send_file, current_app, make_response, jsonify
+from flask import send_file, current_app
 from flask_restplus import Resource
-from config import db
 from .api_model import player_api
-from auth.permissions import account_activation_required
 from flask_jwt_extended import jwt_required
 from compeition.models import Competition
 from .player_marshmallow import PlayerSchema
 
 
-@player_api.route('/media/player/<path:path>')
+@player_api.route('/media/<path:path>')
 class MediaPlayer(Resource):
     def get(self, path):
-        if path.endswith('.jpg'):
-            img_path = current_app.config['MEDIA_ROOT'] + "/player/" + path
-            try:
-                return send_file(img_path, mimetype='image/jpg')
-            except FileNotFoundError:
-                response = make_response(jsonify({"detail": "404 NOT FOUND"}), 404)
-                return response
-        response = make_response(jsonify({"detail": "400 BAD REQUEST"}), 400)
-        return response
+        if not path.endswith('.png'):
+            return {"message": "not found"}, 404
+
+        img_path = current_app.config['MEDIA_ROOT'] + '/' + path
+        try:
+            return send_file(img_path, mimetype='image/png')
+        except FileNotFoundError:
+            img_path = current_app.config['MEDIA_ROOT'] + '/default.png'
+            return send_file(img_path, mimetype='image/png')
+
 
 @player_api.route('/<int:competition_id>/pick-squad')
 class CompetitionPlayers(Resource):
