7c2592cfb6613dfe2d27985fcdfcfcec80c09aae
alirezaSafi <ali02reza78@gmail.com>
2020-05-01 02:00:02 +0430

    Add bundesliga pipline

--- scrap_images/pipelines.py
+++ scrap_images/pipelines.py
@@ -16,7 +16,7 @@ class PremiesLeaguePipeline(ImagesPipeline):
         return image, buf
 
     def get_media_requests(self, item, info):
-        yield scrapy.Request(item.get("image_urls"))
+        yield scrapy.Request(item.get("image_url"))
 
     def file_path(self, request, response=None, info=None):
         player_code = request.url.split('/')[-1]
@@ -28,3 +28,14 @@ class LaligaPipeline(PremiesLeaguePipeline):
     def file_path(self, request, response=None, info=None):
         player_code = request.url.split('/')[-3] + ".png"
         return player_code
+
+
+class BundesligaPipline(PremiesLeaguePipeline):
+
+    def get_media_requests(self, item, info):
+        request = scrapy.Request(url=item.get("image_url"))
+        request.meta['imag_name'] = item.get("image_name")
+        return request
+
+    def file_path(self, request, response=None, info=None, item=None):
+        return '%s.png' % (request.meta['imag_name'])
