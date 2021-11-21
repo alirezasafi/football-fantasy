3277ea2cba8329e006327f138c3cbb3ba91610ca
alirezaSafi <ali02reza78@gmai.com>
2021-11-21 18:18:14 +0330

    Update docker-compose

--- manage.py
+++ manage.py
@@ -1,11 +1,12 @@
 from config import db
-from flask_migrate import Migrate, MigrateCommand
+# from flask_migrate import Migrate, MigrateCommand
+from flask_migrate import Migrate
 from router import app
 from flask_script import Manager, Command
 
 migrate = Migrate(db=db, app=app)
 manager = Manager(app)
-manager.add_command('db', MigrateCommand)
+# manager.add_command('db', MigrateCommand)
 
 
 @manager.command
